516700e06aaf0f58e68907901b2658b0c0d8bd12
Suflistico <ppanguinao.p@gmail.com>
2024-08-14 00:23:26 -0400

    incluir iconos de pizza

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-/*import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import PizzaDetails from './components/PizzaDetails'; 
-import Cart from './components/Cart';
-import Navbar from './components/Navbar';
-import { CartProvider } from './context/CartContext';
-import './App.css'; 
-import './PizzaDetails.css';
-
-const App = () => {
-  return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <div className="app-background">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pizza/:id" element={<PizzaDetails />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
-  );
-};
-
-export default App;*/
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import PizzaDetails from './components/PizzaDetails'; 
 import Cart from './components/Cart';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';  // Importa el Footer
+import Footer from './components/Footer';
 import { CartProvider } from './context/CartContext';
 import './App.css'; 
 import './PizzaDetails.css';
--- src/components/PizzaDetails.js
+++ src/components/PizzaDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import pizzasData from '../data/pizzas.json';
 import '../PizzaDetails.css';
@@ -20,6 +20,42 @@ function PizzaDetails() {
         <p className="pizza-description">{pizza.desc}</p>
         <p className="pizza-ingredients">Ingredientes: {pizza.ingredients.join(', ')}</p>
         <p className="pizza-price">Precio: ${pizza.price}</p>
+        <button onClick={() => navigate('/')} className="back-button">Volver</button>
+      </div>
+    </div>
+  );
+}
+
+export default PizzaDetails;*/
+
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import pizzasData from '../data/pizzas.json';
+import { FaPizzaSlice } from 'react-icons/fa';
+import '../PizzaDetails.css';
+
+function PizzaDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const pizza = pizzasData.find((p) => p.id === id);
+
+  if (!pizza) {
+    return <h2>Pizza no encontrada</h2>;
+  }
+
+  return (
+    <div className="pizza-details-container">
+      <div className="pizza-card-details">
+        <h1>{pizza.name}</h1>
+        <img src={pizza.img} alt={pizza.name} className="pizza-details-img" />
+        {/* Mostrar un ícono de pizza por cada ingrediente */}
+        <div className="pizza-ingredients-icons">
+          {pizza.ingredients.map((ingredient, index) => (
+            <div key={index} className="ingredient-icon">
+              <FaPizzaSlice size={24} />
+            </div>
+          ))}
+        </div>
         <button onClick={() => navigate('/')} className="back-button">Volver</button>
       </div>
     </div>
