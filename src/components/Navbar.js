164a18d21e95dd36dddbfea735fe653d95be0f1e
suflistico <ppanguinao.p@gmail.com>
2024-08-22 13:11:45 -0400

    incorporacion de cantidad de pizza agregadas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+/*import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 import '../Navbar.css';
@@ -18,6 +18,33 @@ const Navbar = () => {
           </Link>
         </div>
       )}
+    </nav>
+  );
+};
+
+export default Navbar;*/
+
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import '../Navbar.css';
+import { CartContext } from '../context/CartContext';
+
+const Navbar = () => {
+  const { totalItems, clickCount } = useContext(CartContext); // Obtener las unidades de pizza agregadas
+
+  return (
+    <nav className="navbar">
+      <div className="title">Bienvenido a......Pizzeria Paolo</div>
+      {totalItems > 0 && (
+        <div className="cart">
+          <Link to="/cart" className="cartLink">
+            <FaShoppingCart size={24} />
+            <span className="cartCount">{totalItems}</span>
+          </Link>
+        </div>
+      )}
+      <div className="clickCounter"> {clickCount}</div> {/* MOstrar Cantidad de Pizza */}
     </nav>
   );
 };
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -5,6 +5,7 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
+  const [clickCount, setClickCount] = useState(0); // Nuevo estado para el contador de clics
 
   useEffect(() => {
     setTotalItems(cart.reduce((sum, pizza) => sum + pizza.quantity, 0));
@@ -17,20 +18,27 @@ export const CartProvider = ({ children }) => {
     } else {
       setCart([...cart, { ...pizza, quantity: 1 }]);
     }
+    setClickCount(clickCount + 1); // Incrementa el contador de pizzas por cada vez que se agrega una pizza
   };
 
   const updateQuantity = (id, quantity) => {
     setCart(cart.map(item => item.id === id ? { ...item, quantity } : item));
+    const previousQuantity = cart.find(item => item.id === id)?.quantity || 0;
+    setClickCount(clickCount + (quantity - previousQuantity)); // Ajusta el contador basado en la diferencia de cantidad
   };
 
   const removeFromCart = (id) => {
+    const removedPizza = cart.find(item => item.id === id);
+    if (removedPizza) {
+      setClickCount(clickCount - removedPizza.quantity); // Disminuye las unidades de pizza
+    }
     setCart(cart.filter(item => item.id !== id));
   };
 
   const total = cart.reduce((sum, pizza) => sum + (pizza.price * pizza.quantity), 0);
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, updateQuantity, removeFromCart, total, totalItems }}>
+    <CartContext.Provider value={{ cart, addToCart, updateQuantity, removeFromCart, total, totalItems, clickCount }}>
       {children}
     </CartContext.Provider>
   );
