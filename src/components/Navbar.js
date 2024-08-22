6dae09cbecb2340d7d3272868cf2fd5227dba23a
suflistico <ppanguinao.p@gmail.com>
2024-08-22 13:34:48 -0400

    actualizacion de iconos de carrito de compra

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,3 @@
-/*import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
-import '../Navbar.css';
-import { CartContext } from '../context/CartContext';
-
-const Navbar = () => {
-  const { totalItems } = useContext(CartContext);
-
-  return (
-    <nav className="navbar">
-      <div className="title">Bienvenido a......Mamma Pizza</div>
-      {totalItems > 0 && ( 
-        <div className="cart">
-          <Link to="/cart" className="cartLink">
-            <FaShoppingCart size={24} />
-            <span className="cartCount">{totalItems}</span>
-          </Link>
-        </div>
-      )}
-    </nav>
-  );
-};
-
-export default Navbar;*/
-
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
@@ -31,20 +5,18 @@ import '../Navbar.css';
 import { CartContext } from '../context/CartContext';
 
 const Navbar = () => {
-  const { totalItems, clickCount } = useContext(CartContext); // Obtener las unidades de pizza agregadas
+  const { totalItems, clickCount } = useContext(CartContext);
 
   return (
     <nav className="navbar">
       <div className="title">Bienvenido a......Pizzeria Paolo</div>
-      {totalItems > 0 && (
-        <div className="cart">
-          <Link to="/cart" className="cartLink">
-            <FaShoppingCart size={24} />
-            <span className="cartCount">{totalItems}</span>
-          </Link>
-        </div>
-      )}
-      <div className="clickCounter"> {clickCount}</div> {/* MOstrar Cantidad de Pizza */}
+      <div className="cart">
+        <Link to="/cart" className="cartLink">
+          <FaShoppingCart size={24} />
+          <span className="cartCount">{totalItems}</span>
+        </Link>
+      </div>
+      <div className="clickCounter">{clickCount}</div>
     </nav>
   );
 };
